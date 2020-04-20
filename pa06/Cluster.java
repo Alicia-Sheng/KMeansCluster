4062195466554c4e14d1a13f12b29c3ca2a89844
emmaxyt <63563386+emmaxyt@users.noreply.github.com>
2020-04-20 19:01:39 -0400

    Update Cluster.java

--- pa06/Cluster.java
+++ pa06/Cluster.java
@@ -6,7 +6,6 @@ import java.util.Random;
 /**
  * A cluster is a cluster point (which is itself a sample)
  * and a list of Samples (the one's closest to that sample point, ideally).
- * @author presenting
  *
  */
 public class Cluster {
